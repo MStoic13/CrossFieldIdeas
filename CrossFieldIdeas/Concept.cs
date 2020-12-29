92687fc1b64c08a4331da28689fac990660f696d
MStoic13 <34403668+MStoic13@users.noreply.github.com>
2020-12-29 00:40:01 -0800

    add concept

---
+++ CrossFieldIdeas/Concept.cs
@@ -0,0 +1,10 @@
+﻿namespace CrossFieldIdeas
+{
+    public class Concept
+    {
+        public string Name { get; }
+        public string Domain { get; }
+        public string Definition { get; }
+        public string WikipediaLink { get; }
+    }
+}
--- CrossFieldIdeas/Program.cs
+++ CrossFieldIdeas/Program.cs
@@ -10,6 +10,7 @@ namespace CrossFieldIdeas
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
